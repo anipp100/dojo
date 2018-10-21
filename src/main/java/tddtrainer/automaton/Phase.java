796f20641f27f1003fea2da50822dcfdec62ef3a
Andre Ippisch <andre.ippisch@uni-duesseldorf.de>
2018-10-21 16:36:59 +0200

    Add doc for phases

--- src/main/java/tddtrainer/automaton/Phase.java
+++ src/main/java/tddtrainer/automaton/Phase.java
@@ -1,12 +1,10 @@
 package tddtrainer.automaton;
 
-/**
- * RED: A new failing test should be created GREEN: Code to pass the failing
- * test should be entered REFACTOR: Code and tests can be improved
- *
- */
 public enum Phase {
 
-    RED, GREEN, REFACTOR, RETROSPECT
+    RED, // a new failing test should be created
+    GREEN, // code to pass the failing test should be entered
+    REFACTOR, // code and tests can be improved
+    RETROSPECT // review the code and go back to phase RED
 
 }
